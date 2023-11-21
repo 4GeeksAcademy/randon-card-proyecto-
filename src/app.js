b8e45ea5e5776417caa3f0c3dd2baed778cbccca
Zurda13 <144481470+Zurda13@users.noreply.github.com>
2023-11-21 20:43:23 +0000

    proyecto carta random

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let palos = ["♥", "♠", "♦", "♣"];
+  let numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let randomPalos = Math.floor(Math.random() * palos.length);
+  let randomNumeros = Math.floor(Math.random() * numeros.length);
+  let elementPalos = document.getElementsByClassName("palos");
+  elementPalos[0].innerHTML = palos[randomPalos];
+  elementPalos[1].innerHTML = palos[randomPalos];
+  if (palos[randomPalos] === "♥" || palos[randomPalos] === "♦") {
+    elementPalos[0].style.color = "red";
+    elementPalos[1].style.color = "red";
+  } else {
+    elementPalos[0];
+    elementPalos[1];
+  }
+  document.querySelector(".numeros").innerHTML = numeros[randomNumeros];
 };
